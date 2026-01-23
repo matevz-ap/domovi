da39c793e8b50c0546a9cf6be89b9a70083767d3
matevz-ap <47795510+matevz-ap@users.noreply.github.com>
2026-01-23 23:25:31 +0100

    deploy to pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  site: 'https://domovi.mapsoft.net',
+  base: '/domovi',
   vite: {
     plugins: [tailwindcss()],
   },
