7b191ab4724faab1ed41992a19585d050422d11b
matevz-ap <47795510+matevz-ap@users.noreply.github.com>
2026-01-23 23:28:49 +0100

    remove base

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: 'https://domovi.mapsoft.net',
-  base: '/domovi',
   vite: {
     plugins: [tailwindcss()],
   },
